1bec760439f58a2aa866e6771fc9710fad5eb103
Bogdan Prokopenko <bogdanprokopenko67@gmail.com>
2024-07-25 18:41:14 +0300

    added fakeStoreApi

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,16 +1,37 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from '@angular/core';
+import {StoreService} from "../../../../services/store.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.scss'
 })
-export class FiltersComponent {
+export class FiltersComponent implements  OnInit, OnDestroy {
   @Output() showCategory = new EventEmitter<string>();
-  categories = ['shoes', 'sports'];
+  categoriesSubscription: Subscription |undefined;
+
+  constructor(private storeService: StoreService) {
+
+  }
+
+  categories: Array<string> | undefined;
 
   onShowCategory(category:string) {
     this.showCategory.emit(category)
+  }
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService.getAllCategories()
+        .subscribe((response) => {
+          this.categories = response;
+        })
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {CartService} from "../../services/cart.service";
 import {Product} from "../../models/product/product.module";
+import {Subscription} from "rxjs";
+import {StoreService} from "../../services/store.service";
 
 const ROWS_HEIGHT: {[id: number]: number} = { 1: 400, 3: 335, 4: 350}
 
@@ -9,20 +11,58 @@ const ROWS_HEIGHT: {[id: number]: number} = { 1: 400, 3: 335, 4: 350}
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-  constructor(private cartService: CartService) {
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor(
+      private cartService: CartService,
+      private storeService: StoreService,
+  ) {
   }
 
   cols = 3;
   rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
+  products: Array<Product> | undefined;
+  sort = 'desc';
+  count = '12';
+  productsSubscription: Subscription | undefined;
+
+  ngOnDestroy(): void {
+    if (this.productsSubscription) {
+      this.productsSubscription.unsubscribe();
+    }
+    throw new Error('Method not implemented.');
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  onItemsCountChange(newCount: number) {
+    this.count = newCount.toString();
+    this.getProducts()
+  }
+
+  onSortChange(newSort: string) {
+    this.sort = newSort;
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productsSubscription = this.storeService.getAllProducts(this.count, this.sort, this.category)
+        .subscribe((_products) => {
+          this.products = _products;
+        })
+  }
+
+
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
     this.rowHeight = ROWS_HEIGHT[this.cols]
   }
 
   onShowCategory(newCategory: string) {
-    this.category = newCategory
+    this.category = newCategory;
+    this.getProducts()
   }
 
   onAddToCart(product: Product) {
@@ -34,5 +74,7 @@ export class HomeComponent {
       id: product.id
     })
   }
+
+
 
 }
